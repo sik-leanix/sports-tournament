a158bea414e2baa7e366818539a799b7a5e9ab41
Sidney Klass <sidney.klass@leanix.net>
2021-11-26 16:49:56 +0100

    Improve website design

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,46 @@
 import express, { Application, Request, Response } from 'express'
 import { pg } from './db/database';
+import { renderFile, RenderOptions } from 'twig';
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app: Application = express()
-
+app.use(express.static('../frontend/static/'));
 const port: number = 3001
 
 interface Tournament {
     id: string;
     name: string;
+    'joining-code': number;
 }
+
+app.get('/', async (req: Request, res: Response) => {
+
+    const query = pg.select("id", "name").from<Tournament>("tournament");
+    const tournaments = await query;
+    renderFile("../frontend/index.html", { tournaments: tournaments } as RenderOptions, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.get('/', async (req: Request, res: Response) => {
+
+    const query = pg.select("id", "name").from<Tournament>("tournament");
+    const tournaments = await query;
+    renderFile("../frontend/joinTournament.html", { tournaments: tournaments } as RenderOptions, (err, result) => {
+        res.send(result);
+    });
+});
 
 app.get('/tournaments', async(req: Request, res: Response) => {
     const query = pg.select("id", "name").from<Tournament>("tournament");
     const tournaments = await query;
     
-    res.json( { tournaments })
-})
+    res.json( { tournaments });
+});
 
 app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`)
-})
+    console.log(`App is listening on port ${port} !`);
+});
+
