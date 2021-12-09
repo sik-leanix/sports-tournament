98cb1a089f4e6f26842ba62e8bd661a1b6dad8a0
konstantintieber <46342664+konstantintieber@users.noreply.github.com>
2021-12-09 15:59:28 +0000

    Deploying to gh-pages from @ sik-leanix/sports-tournament@3629b4dc6361cbe370b81071dfff7d0686859a13 🚀

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,9 +6,9 @@
 // -----------------------------------------------------------------------------
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
+var del = require('del');
 var autoprefixer = require('gulp-autoprefixer');
-var sassdoc = require('sassdoc');
 var browserSync = require('browser-sync').create();
 var nunjucksRender = require('gulp-nunjucks-render');
 var concat      = require('gulp-concat');
@@ -22,10 +22,8 @@ var siteOutput = './dist';
 var input = './scss/*.scss';
 var inputMain = './scss/main.scss';
 var output = siteOutput + '/css';
-var inputTemplates = './pages/*.html';
+var inputTemplates = './templates/*.njk';
 var sassOptions = { outputStyle: 'expanded' };
-var autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
-var sassdocOptions = { dest: siteOutput + '/sassdoc' };
 
 
 // -----------------------------------------------------------------------------
@@ -36,7 +34,7 @@ gulp.task('sass', function() {
   return gulp
     .src(inputMain)
     .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(autoprefixer())
     .pipe(gulp.dest(output))
     .pipe(browserSync.stream());
 });
@@ -70,17 +68,6 @@ gulp.task('nunjucks', function() {
   .pipe(gulp.dest(siteOutput))
 });
 
-// -----------------------------------------------------------------------------
-// Sass documentation generation
-// -----------------------------------------------------------------------------
-
-gulp.task('sassdoc', function() {
-  return gulp
-    .src(input)
-    .pipe(sassdoc(sassdocOptions))
-    .resume();
-});
-
 
 // -----------------------------------------------------------------------------
 // Watchers
@@ -89,14 +76,14 @@ gulp.task('sassdoc', function() {
 gulp.task('watch', function() {
     // Watch the sass input folder for change,
     // and run `sass` task when something happens
-    gulp.watch(input, ['sass']).on('change', function(event) {
+    gulp.watch(input, gulp.series('sass')).on('change', function(event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
-    gulp.watch('./js/*', ['scripts']).on('change', browserSync.reload);
+    gulp.watch('./js/*', gulp.series('scripts')).on('change', browserSync.reload);
 
     // Watch nunjuck templates and reload browser if change
-    gulp.watch(inputTemplates, ['nunjucks']).on('change', browserSync.reload);
+    gulp.watch(inputTemplates, gulp.series('nunjucks')).on('change', browserSync.reload);
 
 });
 
@@ -113,9 +100,30 @@ gulp.task('browser-sync', function() {
   });
 });
 
+//
+
+gulp.task('clean', function() {
+  return del(['dist'])
+});
+
 
 // -----------------------------------------------------------------------------
 // Default task
 // -----------------------------------------------------------------------------
 
-gulp.task('default', ['sass', 'nunjucks', 'img', 'scripts', 'watch', 'browser-sync']);
+gulp.task('default', gulp.series('sass', 'nunjucks', 'scripts', 'watch'),
+function() {
+  browserSync.init({
+    server: {
+      baseDir: siteOutput
+    }
+  });
+});
+
+// Build task
+
+gulp.task('build', gulp.series('clean', gulp.parallel('sass', 'nunjucks', 'scripts')),
+  function() {
+    console.log('built your app to /dist');
+  }
+);
