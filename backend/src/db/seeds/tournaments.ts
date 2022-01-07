92a14f445307520fb824381fb7bd5269d5b25284
Sidney Klass <sidney.klass@leanix.net>
2022-01-07 14:50:03 +0100

    Correct table name in seed
    Now it is possible to insert data into the table

--- backend/src/db/seeds/tournaments.ts
+++ backend/src/db/seeds/tournaments.ts
@@ -2,11 +2,12 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("tournaments").del();
+    await knex("tournament").del();
 
     // Inserts seed entries
-    await knex("tournaments").insert([
+    await knex("tournament").insert([
         { id: 1, name: "Wimbledon" },
-        { id: 2, name: "Australian Open" }
+        { id: 2, name: "Australian Open" },
+        { id: 3, name: "US Open" },
     ]);
 };
