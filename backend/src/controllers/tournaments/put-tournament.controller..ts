26c4ec5e5850302cdeb71ce4fc953f8394c25012
Sidney Klass <sidney.klass@leanix.net>
2022-02-18 12:28:42 +0100

    Remove unused console.log

--- backend/src/controllers/tournaments/put-tournament.controller..ts
+++ backend/src/controllers/tournaments/put-tournament.controller..ts
@@ -4,7 +4,6 @@ import { updateTournament } from './tournament.dao';
 export const putTournamentController =  async(req: Request, res: Response) => {
     const id = req.params.id;
     const body = req.body;
-    console.log(body.id);
     if (body.id) {
         res.status(405);
         res.end();
