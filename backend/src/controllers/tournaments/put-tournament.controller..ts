88e6b8578ec3d94f20562e71d10e56d7dc434506
Sidney Klass <sidney.klass@leanix.net>
2022-02-18 12:27:17 +0100

    Forbid user to change tournamet id

--- backend/src/controllers/tournaments/put-tournament.controller..ts
+++ backend/src/controllers/tournaments/put-tournament.controller..ts
@@ -4,7 +4,13 @@ import { updateTournament } from './tournament.dao';
 export const putTournamentController =  async(req: Request, res: Response) => {
     const id = req.params.id;
     const body = req.body;
-    const updatedBody = await updateTournament(body, id);
-    res.status(200);
-    res.json(updatedBody);
+    console.log(body.id);
+    if (body.id) {
+        res.status(405);
+        res.end();
+    } else {
+        const updatedBody = await updateTournament(body, id);
+        res.status(200);
+        res.json(updatedBody);
+    }
 };
