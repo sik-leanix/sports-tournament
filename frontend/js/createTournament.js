4fd8a668ecd5062397f3baca9aea224f26a64a66
Sidney Klass <sidney.klass@leanix.net>
2022-02-23 14:45:02 +0100

    Change two step form to a one step form
    The second form is now fully removed. The form moves to the admin site where the admin can enter the data

--- frontend/js/createTournament.js
+++ frontend/js/createTournament.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("tournament-form");
-const button = document.getElementById("nextButton");
+const button = document.getElementById("submitButton");
 const div = document.getElementById("createTournamentDiv");
 
 const inputName = document.getElementById("inputName");
@@ -9,8 +9,6 @@ const inputAdminCode = document.getElementById("inputAdminCode");
 const spanName = document.getElementById("inputValidationName");
 const spanDescription = document.getElementById("inputValidationDescription");
 const spanAdminCode = document.getElementById("inputValidationAdminCode");
-
-let formValues = {}; 
 
 let inputCorrectName = false;
 let inputCorrectDescription = false;
@@ -22,17 +20,27 @@ const checkIfAllInputsAreCorrect = () => {
     } else {
         return false
     }
-}
-
+};
 
 const validationValueLength = (inputElement, spanElement, message) => {
     const value = inputElement.value;
-    if (value.length < 3) {
+    const colorInputRed = () => {
         inputElement.style.borderWidth = "2px";
         inputElement.style.borderColor = "red";
-        spanElement.textContent = "It must contain at least three characters";
         spanElement.style.color = "red";
         button.disabled = true;
+    }
+    if (message === "name" && value.length > 40) {
+        colorInputRed();
+        spanElement.textContent = "The name can be a maximum of 40 characters";
+        inputCorrectName = false;
+    } else if (message === "description" && value.length > 150) {
+        colorInputRed();
+        spanElement.textContent = "The name can be a maximum of 150 characters";
+        inputCorrectDescription = false;
+    } else if (value.length < 3) {
+        colorInputRed();
+        spanElement.textContent = "It must contain at least three characters";
         if (message === "name") {
             inputCorrectName = false;
         } else {
@@ -86,63 +94,48 @@ inputName.addEventListener("input", validationValueLengthWithName);
 inputDescription.addEventListener("input", validationValueLengthWithDescription);
 inputAdminCode.addEventListener("input", validateAdminCode);
 
-button.addEventListener("click", () => {
+const createTournamentCompletion = () => {
+    div.innerHTML = `
+    <h3 style="display: block; text-align: center; padding-top: 10vh; margin: 0 0 15vh 0"> You successfully created a tournament! </h3>
+    <p class="tournamentComplete">You can reach the tournament under: </p>
+    `;
+};
+
+
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
     const name = form.name.value;
     const description = form.description.value;
     const adminCode = form.admin_code.value;
 
     if (checkIfAllInputsAreCorrect()) {
-        formValues = {
+        const formValues = {
             name: name,
             description: description,
             admin_code: adminCode
         };
-        formSite2();
-    }
-})
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    formValues.url_slug = form.url_slug.value;
-    formValues.player_code = form.player_code.value;
-
-    const url = 'http://localhost:8000/tournaments';
-
-    async function post(url) {
-        try {
-            const reponse = await fetch(url, {
-                method: 'post',
-                headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formValues)
-            });
-            const data = await reponse.json();
-        } catch(error) {
-            console.error(error);
+        async function post(url) {
+            try {
+                const reponse = await fetch(url, {
+                    method: 'post',
+                    headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formValues)
+                });
+                const data = await reponse.json();
+            } catch(error) {
+                console.error(error);
+            }
         }
+
+        const url = 'http://localhost:8000/tournaments';
+        post(url);
+        createTournamentCompletion();
     }
-    post(url);
-    createTournamentCompletion();
 });
 
-const formSite2 = () => {
-    form.innerHTML = `
-    <label for="url_slug"> Link for player registration:</label>
-    <input type="text" name="url_slug" class="form-control" placeholder="https://sik-leanix.github.io/tournaments/..." required>
-    <span>This link is required to register for the tournament.</span>
-    <label for="player_code"> Players Code: </label>
-    <input type="text" class="form-control" name="player_code" placeholder="Player Code..." required>
-    <span>This code can be shared with the participants. It is necessary for the players in the registration prozess.</span>
-    <button type="submit" class="btn btn-primary" style="display: block; margin-top: 10vh">Create Tournament</button>
-    `
-}
-
-const createTournamentCompletion = () => {
-    div.innerHTML = `
-    <h3 style="display: block; text-align: center; padding-top: 10vh; margin: 0 0 15vh 0"> You successfully created a tournament! </h3>
-    <p class="tournamentComplete">You can reach the tournament under: </p>
-    <a class="tournamentComplete" href="https://sik-leanix.github.io/tournaments/${formValues.url_slug}"> https://sik-leanix.github.io/tournaments/${formValues.url_slug} </a>
-    `;
-};
