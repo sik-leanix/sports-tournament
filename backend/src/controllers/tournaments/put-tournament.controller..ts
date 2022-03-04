58a17379f5ea497f4aa26771fe1ed1bbc77c381d
Sidney Klass <sidney.klass@leanix.net>
2022-03-04 12:02:50 +0100

    Change kind of error if user wants to update id

--- backend/src/controllers/tournaments/put-tournament.controller..ts
+++ backend/src/controllers/tournaments/put-tournament.controller..ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import { updateTournament } from './tournament.dao';
+import { BadRequest } from 'express-openapi-validator/dist/openapi.validator';
 
 export const putTournamentController =  async(req: Request, res: Response) => {
     const id = req.params.id;
     const body = req.body;
     if (body.id) {
-        res.status(405);
+        res.status(400).json(new BadRequest({ path: "/tournament", message: 'Changing the ID is not allowed' }));
         res.end();
     } else {
         const updatedBody = await updateTournament(body, id);
