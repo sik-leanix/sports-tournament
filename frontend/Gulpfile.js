2417e4c49ec1f65e64a0f4c53376c6296cbd1fb1
Konstantin Tieber <konstantin.tieber@leanix.net>
2022-03-04 13:52:51 +0100

    Setup local single page routing for /tournament/route

--- frontend/Gulpfile.js
+++ frontend/Gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // -----------------------------------------------------------------------------
 // Dependencies
 // -----------------------------------------------------------------------------
@@ -11,9 +10,7 @@ var del = require('del');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var nunjucksRender = require('gulp-nunjucks-render');
-var concat      = require('gulp-concat');
 var siteOutput = './dist';
-
 
 // -----------------------------------------------------------------------------
 // Configuration
@@ -22,15 +19,14 @@ var siteOutput = './dist';
 var input = './scss/*.scss';
 var inputMain = './scss/main.scss';
 var output = siteOutput + '/css';
-var inputTemplates = ['./pages/*.njk', './pages/tournament/*.njk' ];
+var inputTemplates = ['./pages/*.njk', './pages/**/*.njk'];
 var sassOptions = { outputStyle: 'expanded' };
-
 
 // -----------------------------------------------------------------------------
 // Sass compilation
 // -----------------------------------------------------------------------------
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp
     .src(inputMain)
     .pipe(sass(sassOptions).on('error', sass.logError))
@@ -39,64 +35,67 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream());
 });
 
-
 // -----------------------------------------------------------------------------
 // Javascript
 // -----------------------------------------------------------------------------
 
-gulp.task('scripts', function() {
-  return gulp.src([
-	  	'js/main.js',
-      'js/createTournament.js'
-  	])
-    .pipe(browserSync.reload({stream:true}))
+gulp.task('scripts', function () {
+  return gulp
+    .src(['js/*.js'])
+    .pipe(browserSync.reload({ stream: true }))
     .pipe(gulp.dest(siteOutput + '/js'));
 });
-
 
 // -----------------------------------------------------------------------------
 // Templating
 // -----------------------------------------------------------------------------
 
-gulp.task('nunjucks', function() {
-  return gulp.src(inputTemplates)
-    .pipe(nunjucksRender({
-      path: ['templates/']
-    }))
+gulp.task('nunjucks', function () {
+  return gulp
+    .src(inputTemplates)
+    .pipe(
+      nunjucksRender({
+        path: ['templates/']
+      })
+    )
     .pipe(gulp.dest(siteOutput));
 });
 
 // Delete dist ouput
 
-gulp.task('clean', function() {
-  return del(['dist'])
+gulp.task('clean', function () {
+  return del(['dist']);
 });
 
 const watchFiles = () => {
-    // Watch the sass input folder for change,
-    // and run `sass` task when something happens
-    gulp.watch(input, gulp.series('sass')).on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+  // Watch the sass input folder for change,
+  // and run `sass` task when something happens
+  gulp.watch(input, gulp.series('sass')).on('change', function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
 
-    gulp.watch('./js/*', gulp.series('scripts')).on('change', browserSync.reload);
+  gulp.watch('./js/*', gulp.series('scripts')).on('change', browserSync.reload);
 
-    // Watch nunjuck templates and reload browser if change
-    gulp.watch(inputTemplates, gulp.series('nunjucks')).on('change', browserSync.reload);
+  // Watch nunjuck templates and reload browser if change
+  gulp.watch(inputTemplates, gulp.series('nunjucks')).on('change', browserSync.reload);
 };
-
-
 
 const startDevServer = () => {
   browserSync.init({
     server: {
       baseDir: siteOutput,
-      port: 4200
+      middleware: function (req, res, next) {
+        // redirect all requests that start with /tournament to the /tournament/index.html
+        // where we use JavaScript to fetch the relevant Torunament data based on the URL segments.
+        if (req.url.startsWith('/tournament')) {
+          req.url = '/tournament/index.html';
+        }
+        return next();
+      }
     }
   });
   watchFiles();
-}
-
+};
 
 // -----------------------------------------------------------------------------
 // Dev server task
@@ -106,8 +105,6 @@ gulp.task('serve', gulp.series('sass', 'nunjucks', 'scripts', startDevServer));
 
 // Build task
 
-gulp.task('build', gulp.series('clean', gulp.parallel('sass', 'nunjucks', 'scripts')),
-  function() {
-    console.log('built your app to /dist');
-  }
-);
+gulp.task('build', gulp.series('clean', gulp.parallel('sass', 'nunjucks', 'scripts')), function () {
+  console.log('built your app to /dist');
+});
--- frontend/dist/js/createTournament.js
+++ frontend/dist/js/createTournament.js
@@ -1,16 +1,16 @@
-const form = document.getElementById("tournament-form");
-const button = document.getElementById("submitButton");
-const div = document.getElementById("createTournamentDiv");
+const form = document.getElementById('tournament-form');
+const button = document.getElementById('submitButton');
+const div = document.getElementById('createTournamentDiv');
 
-const inputUrlSlug = document.getElementById("inputUrlSlug");
-const inputName = document.getElementById("inputName");
-const inputDescription = document.getElementById("inputDescription");
-const inputAdminCode = document.getElementById("inputAdminCode");
+const inputUrlSlug = document.getElementById('inputUrlSlug');
+const inputName = document.getElementById('inputName');
+const inputDescription = document.getElementById('inputDescription');
+const inputAdminCode = document.getElementById('inputAdminCode');
 
-const spanUrlSlug = document.getElementById("inputValidationUrlSlug");
-const spanName = document.getElementById("inputValidationName");
-const spanDescription = document.getElementById("inputValidationDescription");
-const spanAdminCode = document.getElementById("inputValidationAdminCode");
+const spanUrlSlug = document.getElementById('inputValidationUrlSlug');
+const spanName = document.getElementById('inputValidationName');
+const spanDescription = document.getElementById('inputValidationDescription');
+const spanAdminCode = document.getElementById('inputValidationAdminCode');
 
 let inputCorrectName = false;
 let inputCorrectDescription = false;
@@ -18,154 +18,150 @@ let inputCorrectAdminCode = false;
 button.disabled = true;
 
 const checkIfAllInputsAreCorrect = () => {
-    if (inputCorrectName && inputCorrectDescription && inputCorrectAdminCode) {
-        return true
-    } else {
-        return false
-    }
+  if (inputCorrectName && inputCorrectDescription && inputCorrectAdminCode) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const validationValueLength = (inputElement, spanElement, message) => {
-    const value = inputElement.value;
-    const colorInputRed = () => {
-        inputElement.style.borderWidth = "2px";
-        inputElement.style.borderColor = "red";
-        spanElement.style.color = "red";
-        button.disabled = true;
-    }
-    if (message === "name" && value.length > 40) {
-        colorInputRed();
-        spanElement.textContent = "The name can be a maximum of 40 characters";
-        inputCorrectName = false;
-    } else if (message === "description" && value.length > 150) {
-        colorInputRed();
-        spanElement.textContent = "The name can be a maximum of 150 characters";
-        inputCorrectDescription = false;
-    } else if (value.length < 3) {
-        colorInputRed();
-        spanElement.textContent = "It must contain at least three characters";
-        if (message === "name") {
-            inputCorrectName = false;
-        } else {
-            inputCorrectDescription = false;
-        }
+  const value = inputElement.value;
+  const colorInputRed = () => {
+    inputElement.style.borderWidth = '2px';
+    inputElement.style.borderColor = 'red';
+    spanElement.style.color = 'red';
+    button.disabled = true;
+  };
+  if (message === 'name' && value.length > 40) {
+    colorInputRed();
+    spanElement.textContent = 'The name can be a maximum of 40 characters';
+    inputCorrectName = false;
+  } else if (message === 'description' && value.length > 150) {
+    colorInputRed();
+    spanElement.textContent = 'The name can be a maximum of 150 characters';
+    inputCorrectDescription = false;
+  } else if (value.length < 3) {
+    colorInputRed();
+    spanElement.textContent = 'It must contain at least three characters';
+    if (message === 'name') {
+      inputCorrectName = false;
     } else {
-        inputElement.style.borderColor = "";
-        inputElement.style.borderWidth = "";
-        spanElement.textContent = "";
-        if (message === "name") {
-            inputCorrectName = true;
-        } else {
-            inputCorrectDescription = true;
-        }
-        if (checkIfAllInputsAreCorrect()) {
-            button.disabled = false;
-        }
+      inputCorrectDescription = false;
     }
+  } else {
+    inputElement.style.borderColor = '';
+    inputElement.style.borderWidth = '';
+    spanElement.textContent = '';
+    if (message === 'name') {
+      inputCorrectName = true;
+    } else {
+      inputCorrectDescription = true;
+    }
+    if (checkIfAllInputsAreCorrect()) {
+      button.disabled = false;
+    }
+  }
 };
 
 const validateAdminCode = () => {
-    const value = inputAdminCode.value;
-    if (value.length > 7 && value.match(/^\S*$/)) {
-        inputAdminCode.style.borderColor = "";
-        inputAdminCode.style.borderWidth = "";
-        spanAdminCode.textContent = "The code is good!";
-        spanAdminCode.style.color = "green"
-        inputCorrectAdminCode = true;
-        if (checkIfAllInputsAreCorrect()) {
-            button.disabled = false;
-        }
-    } else {
-        inputAdminCode.style.borderWidth = "2px";
-        inputAdminCode.style.borderColor = "red";
-        spanAdminCode.textContent = "Passwords must be at least 8 characters long and must not contain whitespaces.";
-        spanAdminCode.style.color = "red";
-        inputCorrectAdminCode = false;
-        button.disabled = true;
+  const value = inputAdminCode.value;
+  if (value.length > 7 && value.match(/^\S*$/)) {
+    inputAdminCode.style.borderColor = '';
+    inputAdminCode.style.borderWidth = '';
+    spanAdminCode.textContent = 'The code is good!';
+    spanAdminCode.style.color = 'green';
+    inputCorrectAdminCode = true;
+    if (checkIfAllInputsAreCorrect()) {
+      button.disabled = false;
     }
+  } else {
+    inputAdminCode.style.borderWidth = '2px';
+    inputAdminCode.style.borderColor = 'red';
+    spanAdminCode.textContent = 'Passwords must be at least 8 characters long and must not contain whitespaces.';
+    spanAdminCode.style.color = 'red';
+    inputCorrectAdminCode = false;
+    button.disabled = true;
+  }
 };
 
 const validationValueLengthWithName = () => {
-    validationValueLength(inputName, spanName, "name");
-}
+  validationValueLength(inputName, spanName, 'name');
+};
 
 const validationValueLengthWithDescription = () => {
-    validationValueLength(inputDescription, spanDescription, "description");
-}
+  validationValueLength(inputDescription, spanDescription, 'description');
+};
 
-inputName.addEventListener("input", validationValueLengthWithName);
-inputDescription.addEventListener("input", validationValueLengthWithDescription);
-inputAdminCode.addEventListener("input", validateAdminCode);
-inputUrlSlug.addEventListener("input", function () {
-    const value = inputUrlSlug.value;
-    inputUrlSlug.value = value.replace(/\s+/g, '-').toLowerCase();
-    if (inputUrlSlug.value.length < 3 || inputUrlSlug.value.length > 40) {
-        inputUrlSlug.style.borderWidth = "2px";
-        inputUrlSlug.style.borderColor = "red";
-        spanUrlSlug.textContent = "The url slug must be at least 3 and no more than 40 characters long";
-        spanUrlSlug.style.color = "red";
-    } else {
-        inputUrlSlug.style.borderColor = "";
-        inputUrlSlug.style.borderWidth = "";
-        spanUrlSlug.textContent = "";
-    }
-})
-
-
-const urlSlugDiv = document.getElementById("urlSlugDiv");
-
+inputName.addEventListener('input', validationValueLengthWithName);
+inputDescription.addEventListener('input', validationValueLengthWithDescription);
+inputAdminCode.addEventListener('input', validateAdminCode);
+inputUrlSlug.addEventListener('input', function () {
+  const value = inputUrlSlug.value;
+  inputUrlSlug.value = value.replace(/\s+/g, '-').toLowerCase();
+  if (inputUrlSlug.value.length < 3 || inputUrlSlug.value.length > 40) {
+    inputUrlSlug.style.borderWidth = '2px';
+    inputUrlSlug.style.borderColor = 'red';
+    spanUrlSlug.textContent = 'The url slug must be at least 3 and no more than 40 characters long';
+    spanUrlSlug.style.color = 'red';
+  } else {
+    inputUrlSlug.style.borderColor = '';
+    inputUrlSlug.style.borderWidth = '';
+    spanUrlSlug.textContent = '';
+  }
+});
 
 const urlSlugDiv = document.getElementById('urlSlugDiv');
 
 form.addEventListener('submit', async function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (!checkIfAllInputsAreCorrect()) {
-        return;
-    }
+  if (!checkIfAllInputsAreCorrect()) {
+    return;
+  }
 
-    const name = form.name.value;
-    const description = form.description.value;
-    const adminCode = form.admin_code.value;
+  const name = form.name.value;
+  const description = form.description.value;
+  const adminCode = form.admin_code.value;
 
-    const formValues = {
-        name: name,
-        description: description,
-        admin_code: adminCode
-    };
+  const formValues = {
+    name: name,
+    description: description,
+    admin_code: adminCode
+  };
 
   if (urlSlugDiv.style.display === 'block') {
-        formValues.url_slug = form.url_slug.value;
-    }
+    formValues.url_slug = form.url_slug.value;
+  }
 
-    const tournament = await postTournament(formValues);
-    if (tournament.errors) {
-        const message = tournament.errors[0].message;
+  const tournament = await postTournament(formValues);
+  if (tournament.errors) {
+    const message = tournament.errors[0].message;
     if (message.match(/(Key\ )\(url_slug\)=(.+)(?=(already exists.))/) && urlSlugDiv.style.display === 'block') {
       // TODO: show red error message below url slug input "URL slug is already used."
-        } else if (message.match(/(Key\ )\(url_slug\)=(.+)(?=(already exists.))/)) {
+    } else if (message.match(/(Key\ )\(url_slug\)=(.+)(?=(already exists.))/)) {
       urlSlugDiv.style.display = 'block';
-        } 
     }
+  }
 });
 
 async function postTournament(data) {
-    try {
-        // TODO: change to non-localhost URL for production
-        const response = await fetch('http://localhost:8000/tournaments', {
-            method: 'post',
-            headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        });
+  try {
+    // TODO: change to non-localhost URL for production
+    const response = await fetch('http://localhost:8000/tournaments', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
     const tournament = response.json();
-        if (response.status === 201) {
+    if (response.status === 201) {
       window.location.href = '/tournament/' + tournament.url_slug + '/admin';
-        }
+    }
     return tournament;
   } catch (error) {
-        console.error(error);
+    console.error(error);
     return null;
-    }
+  }
 }
