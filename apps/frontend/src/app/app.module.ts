9423d13ca1180ad20a0f9b42058881b718f7f58d
Konstantin Tieber <konstantin.tieber@leanix.net>
2022-04-13 13:38:41 +0200

    Attempt to fix duplicate tournaments segment in url

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: ''}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
