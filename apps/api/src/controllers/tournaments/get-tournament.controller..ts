af6633fe96704a618e58e39c6583d3938565cc10
Sidney Klass <sidney.klass@leanix.net>
2022-06-03 17:14:03 +0200

    Add NotFound error

--- apps/api/src/controllers/tournaments/get-tournament.controller..ts
+++ apps/api/src/controllers/tournaments/get-tournament.controller..ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
+import { NotFound } from 'express-openapi-validator/dist/openapi.validator';
 import { getTournamet } from './tournament.dao';
 
 export const getTournametController = async (req: Request, res: Response) => {
   const urlSlug = req.params.urlSlug;
   const tournament = await getTournamet(urlSlug);
-  res.json(tournament);
+  if (tournament) {
+    res.json(tournament);
+  } else {
+    res.status(404).json(new NotFound({ path: '/tournament', message: 'Tournament not found' }));
+  }
 };
