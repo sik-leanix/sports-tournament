9009867ba7a39aa4d036fe325752fda01e96c620
Sidney Klass <sidney.klass@leanix.net>
2022-06-03 17:14:31 +0200

    Add NotFoundComponent to routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router'; // CLI imports router
+import { NotFoundComponent } from './not-found/not-found.component';
 import { TournamentAdminComponent } from './tournament-admin/tournament-admin-container/tournament-admin.component';
 
 export const routes: Routes = [
   {
     path: ':tournament_url_slug/admin',
     component: TournamentAdminComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
   }
 ];
